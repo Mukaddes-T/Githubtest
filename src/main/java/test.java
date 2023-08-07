7fd66d296f3d41621331152bfb4a390b70862298
Mukaddes Toprak <tavsanmukaddes@gmail.com>
2023-08-07 10:00:02 +0300

    versiyon 3

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,5 +3,7 @@ public class test {
         System.out.println("github deneme 1. versiyon");
 
         System.out.println("merhaba bugun github calisiyoruz");
+
+        System.out.println("java guzeldir ");
     }
 }
